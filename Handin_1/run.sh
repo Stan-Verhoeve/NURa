5cebca9131c66617930cd6be70dea9b43b2e2cb9
Stan Verhoeve <verhoeve@strw.leidenuniv.nl>
2025-02-19 13:42:05 +0100

    Updated run.sh to include figure check and latex compilation

--- Handin_1/run.sh
+++ Handin_1/run.sh
@@ -4,7 +4,18 @@
 set -e
 set -o pipefail
 
-# Command line arguments?
+# Check if folder for figures exists
+if [[ ! -d "figures" ]]; then
+	# If not, create it
+	echo "Creating 'figures' directory..."
+	mkdir figures
+else
+	# If so, clear it
+	echo "'figures' directory already exists. Now clearing..."
+	rm -rf figures/*
+fi
+
+# Do we have command line arguments?
 if [[ -n "$1" ]]; then
 	script="$1"
 
@@ -22,4 +33,26 @@ else
 		echo
 		python3 $script
 	done
+fi
+
+# Check if latex folder exists
+if [[ ! -d "latex" ]]; then
+	echo "No 'latex' direcotry found. Skipping compilation..."
+else
+	# If no latex_out folder, create it
+	if [[ ! -d "latex_out" ]]; then
+		echo "Creating 'latex_out' directory..."
+		mkdir latex_out
+	fi
+	
+	# Check if there are any .tex files
+	if compgen -G "latex/*.tex" > /dev/null; then
+		echo "Creating PDFs from LaTeX files"
+	
+		for file in "latex/*.tex"; do
+			pdflatex -output-dir latex_out $file
+		done
+	else
+		echo "No .tex files found. Skipping compilation..."
+	fi
 fi
