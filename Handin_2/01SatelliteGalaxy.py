0d1fcc1d20a49c67c82d1d1fb7dd021249c145f3
Stan Verhoeve <verhoeve@strw.leidenuniv.nl>
2025-03-15 15:32:21 +0100

    Moved integration to helperscript, added integrand and sanity check

--- Handin_2/01SatelliteGalaxy.py
+++ Handin_2/01SatelliteGalaxy.py
@@ -1,59 +1,56 @@
 import numpy as np
 
-def romberg(func, bounds, m=5, err=False, args=None):
-    # Extract bounds and first step size
-    a, b = bounds
-    h = b - a
-    N = 1
-    
-    # Array to hold integral guesses
-    r = np.zeros(m)
+def n(x, A, Nsat, a, b, c):
+    """
+    Number density profile of satellite galaxies
 
-    # Initial trapezoid (on full domain)
-    r[0] = 0.5 * h * (func(a, *args) + func(b, *args))
+    Parameters
+    ----------
+    x : float | ndarray
+        Radius in units of virial radius; x = r / r_virial
+    A : float
+        Normalisation
+    Nsat : float
+        Average number of satellites
+    a : float
+        Small-scale slope
+    b : float
+        Transition scale
+    c : float
+        Steepness of exponential drop-off
 
-    for i in range(1,m):
-        hi = h * 2**(-i)
-        Delta = 2 * hi
-        N *= 2
-
-        # New points to evaluate
-        newx = np.arange(a + hi, b, Delta)
-        newy = func(newx, *args)
-        
-        # New estimate
-        r[i] = 0.5 * (r[i-1] + Delta * np.sum(newy))
-    
-    # Combine estimates iteratively
-    for i in range(m):
-        for j in range(1,m-i):
-            denom_inv = (4 ** (i+1) - 1) ** (-1)
-            r[j-1] = ((4 ** (i+1)) * r[j] - r[j-1]) * denom_inv
-    
-    if err:
-        return r[0], abs(r[0] - r[1])
-    return r[0]
-
-def n(x, a, b, c):
-    return ((x / b) ** (a - 3)) * np.exp(-(x / b) ** c)
-
-def integrand(x, *args):
-    return 4 * np.pi * x**2 * n(x, *args)
+    Returns
+    -------
+    float | ndarray
+        Same type and shape as x. Number density of satellite galaxies
+        at given radius x.
+    """
+    return A * Nsat * ((x / b) ** (a - 3)) * np.exp(-(x / b) ** c)
 
 def main():
+    from helperscripts.integrate import romberg
+
+    # Default values given in problemset
     a=2.4
     b=0.25
     c=1.6
+    Nsat = 100
     bounds = (1e-4, 5)
     
-    result, err = romberg(integrand, bounds, m=10, err=True, args=(a,b,c))
-    A = result ** (-1)
-    print(result, err)
-    from scipy.integrate import quad
+    # 1D integrand to solve for.
+    integrand = lambda x, *args: 4 * np.pi * x**2 * n(x, 1, 1, *args)
+    result, err = romberg(integrand, bounds, m=10, args=(a,b,c), err=True)
 
-    print(quad(integrand, *bounds, args=(a,b,c)))
+    # Normalisation
+    A = 1. / result
+    print(f"Normalisation constant: A = {A}")
 
-    print(A)
-    
+    # Validate that number density profile integrates to Nsat
+    integrand = lambda x, *args: 4 * np.pi * x**2 * n(x, *args)
+    integrated_Nsat = romberg(integrand, bounds, m=10, args=(A, Nsat, a, b, c))
+    print("\nSanity check")
+    print(f"∫∫∫n(x) dV = ⟨Nsat⟩ : {np.isclose(Nsat, integrated_Nsat)}")
+
+
 if __name__ in ("__main__"):
     main()
