f9bb73650fe52a242c1b72d3d356bfd248f2a335
Stan Verhoeve <verhoeve@strw.leidenuniv.nl>
2025-03-17 15:21:33 +0100

    Applied formatting

--- Handin_2/helperscripts/random.py
+++ Handin_2/helperscripts/random.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+
 class MWC:
     def __init__(self, seed, a):
-        if not 0 < seed < 2 ** 32:
+        if not 0 < seed < 2**32:
             raise ValueError("Seed should be 0 < seed < 2**32.")
 
         self.state = np.uint64(seed)
         self.a = np.uint64(a)
-    
+
     def next(self):
         """
         Get next pseudo-random number in sequence
@@ -19,7 +20,7 @@ class MWC:
         """
         self.state = self.a * (self.state & (2**32 - 1)) + (self.state >> 32)
         return self.state
-    
+
     def random(self):
         return (self.next() & (2**32 - 1)) / 2**32
 
@@ -30,11 +31,12 @@ def xorshift(seed, a1, a2, a3):
     a2 = np.uint64(a2)
     a3 = np.uint64(a3)
 
-    state ^= (state >> a1)
-    state ^= (state << a2)
-    state ^= (state >> a3)
+    state ^= state >> a1
+    state ^= state << a2
+    state ^= state >> a3
 
     return state
+
 
 def LCG(seed, a, c, m):
     state = np.uint64(seed)
@@ -80,15 +82,15 @@ class random:
         self.lcg_state = out
 
         return np.uint32(out)
-    
+
     def random(self):
         # Divide by 2 ** 32 to get in [0, 1)
-        return self.next() / 2 ** 32
-    
+        return self.next() / 2**32
+
     def uniform(self, low=0, high=1, size=1):
         """
         Generate array of uniformly distributed numbers in the range [low, high)
-        
+
         Parameters
         ----------
         low : float
