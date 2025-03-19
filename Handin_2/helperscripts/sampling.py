3fa57a3819e1edd4751f5db45abb8eccc04c4abf
Stan Verhoeve <s_verhoeve@hotmail.nl>
2025-03-19 14:23:54 +0100

    Now samples in batches

--- Handin_2/helperscripts/sampling.py
+++ Handin_2/helperscripts/sampling.py
@@ -1,23 +1,28 @@
-from .rng import Random
-import numpy as np
+from .random import Random
+from numpy import zeros
 
-def rejection(dist, low, high, Nsamples, args=()):
-    U1 = Random(44)
-    U2 = Random(33)
+def rejection(dist, low, high, Nsamples, seed=None, args=()):
+    if seed:
+        U1 = Random(seed)
+        U2 = Random(seed >> 8)
+    else:
+        U1 = Random()
+        U2 = Random()
     
-    accepted = np.zeros(Nsamples)
+    accepted = zeros(Nsamples)
     n_accepted = 0
 
     while n_accepted < Nsamples:
-        x = U1.uniform(low, high)
-        y = U2.uniform()
-        if x < 3e-3 and y < 3e-2:
-            if y <= dist(x, *args):
-                print(x)
+        x = U1.uniform(low, high, Nsamples)
+        y = U2.uniform(size = Nsamples)
         
-        if y <= dist(x, *args):
-            accepted[n_accepted] = x
-            n_accepted += 1
+        to_accept = y < dist(x, *args)
+        new_x = x[to_accept]
+
+        remaining = Nsamples - n_accepted
+        to_add = min(len(new_x), remaining)
+
+        accepted[n_accepted:n_accepted + to_add] = new_x[:to_add]
+        n_accepted += to_add
 
     return accepted
-
