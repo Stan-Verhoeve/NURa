65599a3cafa96b4011c5504b8aaa7f445c6a1a73
Stan <s_verhoeve@hotmail.nl>
2025-04-08 20:28:34 +0200

    Added type hinting and docstring

--- Handin_3/helperscripts/optimize.py
+++ Handin_3/helperscripts/optimize.py
@@ -1,11 +1,29 @@
 # Golden ratio to 12 decimals
 PHI = 1.618_033_988_749
 
-def find_bracket(func, a, b, args=(), max_iters=100):
+def find_bracket(func: callable, a: float, b: float, args: tuple=(), max_iters: int=100):
     """
     Finds a valid bracket (a,b,c) for function minimalization, where
     f(b) < f(a) and f(b) < f(c), and a < b < c
+    
+    Parameters
+    ----------
+    func : callable
+        Function to find bracket for
+    a : float
+        Initial left side of bracket
+    b : float
+        Initial right side of bracket
+    args : tuple
+        Arguments to be passed to func
+    max_iters : int, optional
+        Maximum number of iterations to find bracket.
+        The default is 100
 
+    Returns
+    -------
+    bracket : tuple
+        Initial bracket for function
     """
     # Get function values at initial two points
     fa = func(a, *args)
