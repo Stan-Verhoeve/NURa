b0bf3308fb27d7783b92a1c670db9e4ceb04722f
Stan <s_verhoeve@hotmail.nl>
2025-04-10 15:38:22 +0200

    Added Levenberg-Marquardt chi2 fitting to the data

--- Handin_3/01SatelliteGalaxy.py
+++ Handin_3/01SatelliteGalaxy.py
@@ -30,7 +30,6 @@ def n(
     """
     return A * Nsat * ((x / b) ** (a - 3)) * np.exp(-((x / b) ** c))
 
-
 def dn_dx(x: np.ndarray, A: float, Nsat: float, a: float, b: float, c: float):
     """
     Derivative of number density provide
@@ -69,8 +68,14 @@ def dn_dx(x: np.ndarray, A: float, Nsat: float, a: float, b: float, c: float):
 
 def main():
     from helperscripts.integrate import romberg
-    from helperscripts.optimize import golden_section
+    from helperscripts.optimize import golden_section, levenberg_marquardt
+    from helperscripts.io import readfile
+    import matplotlib.pyplot as plt
 
+
+    ##########################
+    ## Q1a: finding maximum ##
+    ##########################
     # Default values given in problemset
     a = 2.4
     b = 0.25
@@ -93,17 +98,142 @@ def main():
     xmin = golden_section(func, *bracket, args=(a, b, c), atol=1e-8)
     print(f"Maximum found at x={xmin}")
     print(f"Function value at maximum: N(x) = {N_of_x(xmin, a, b, c)}")
+    
+    
+    ###########################
+    ## Q1b: Gaussian fitting ##
+    ###########################
+    
+    # Create figure
+    fig, axs = plt.subplots(3,2, figsize=(1.5*6.4, 1.5*8.0))
+    
+    for i in range(5):
+        print(f"Currently working on satgals_m1{i+1}.txt")
+        # Reading data
+        radius, nhalo = readfile(f"data/satgals_m1{i+1}.txt")
+        
+        # Binning the data
+        Nbins = 100
+        edges = np.logspace(np.log10(1e-4), np.log10(5), Nbins + 1)
+        centers_log = 0.5 * (np.log10(edges[1:]) + np.log10(edges[:-1]))
+        centers = 10 ** centers_log
+        
+        # Normalised histogram
+        hist = np.histogram(radius, bins=edges)[0]
+        hist_scaled = hist / np.diff(edges) / nhalo
+        
+        # Averagey galaxies per halo
+        Ntest = len(radius) / nhalo
 
-    # TODO: test. Remove before handing in
-    from matplotlib import pyplot as plt
+        
+        #######################
+        ## Levenberg fitting ##
+        #######################
 
-    plt.figure()
-    plt.plot(xx, func(xx, a, b, c))
-    plt.scatter(xmin, func(xmin, a, b, c), c="r")
-    plt.xscale("log")
-    # plt.yscale("log")
-    plt.savefig("figures/tests/01Satellite_test.png")
+        # Gradient wrt model parameters for Gaussian errors
+        def gauss_grad(data, model, sigma, derivatives, p):
+            x, y = data[:,0], data[:,1]
+            f = model(x, *p)
 
+            # Jacobian
+            J = [df(x, *p) for df in derivatives]
+            J = np.stack(J, axis=1)
+            res = (y - f) / sigma**2
+
+            return -2 * J.T @ res
+        
+        # Gradient wrt model parameters for Poissonian errors
+        def poiss_grad(data, model, sigma, derivatives, p):
+            x, y = data[:,0], data[:,1]
+            f = model(x, *p)
+            print(f)
+            print(p)
+            # Jacobian
+            J = [df(x, *p) for df in derivatives]
+            J = np.stack(J, axis=1)
+            res = (y / f - 1)
+            return J.T @ res
+        
+        # logL for Gaussian errors (chiSquared)
+        def logL(data, model, sigma, p):
+            x, y = data[:,0], data[:,1]
+            res = (y - model(x, *p)) / sigma
+            return np.sum(res**2)
+        
+        # logL for Poissonian errors
+        def logL_poisson(data, model, sigma, p):
+            x, y = data[:, 0], data[:, 1]
+            y_model = model(x, *p)
+            return np.sum(y * np.log(y_model + 1e-10) - y_model)
+        
+        
+        ##########################################
+        ## Model and derivatives wrt parameters ##
+        ##########################################
+        def model(x, a, b, c):
+            return 4*np.pi * x**2 * n(x, 1, Ntest, a, b, c)
+        
+        def dn_da(x, a, b, c):
+            return model(x, a, b, c) * np.log(x/b)
+
+        def dn_db(x, a, b, c):
+            return model(x, a, b, c) * (c * (x/b)**c - (a-3)) / b
+        
+        def dn_dc(x, a, b, c):
+            return -model(x, a, b, c) * np.log(x/b) * (x/b)**c
+
+        
+        # Initial guess and data matrix
+        p0 = [1.5, 0.5, 1.5]
+        data = np.stack([centers, hist_scaled], axis=1)
+        
+        # Levenberg-Marquardt fitting procedure
+        params = levenberg_marquardt(data=data,
+                                     model=model,
+                                     sigma=Ntest,
+                                     derivatives=(dn_da, dn_db, dn_dc),
+                                     logL=logL,
+                                     dlogL_dp=gauss_grad,
+                                     p0=p0,
+                                     step=1e-2,
+                                     weight=20,
+                                     max_iters=300,
+                                     atol=0.01)
+        
+        # TODO: Currently compares to curve_fit
+        #       Keep in as comparison? Or remove later?
+        from scipy.optimize import curve_fit
+        popt, pcov = curve_fit(model, data[:,0], data[:,1], p0)
+        
+        print("    Best fitting parameters using Levenberg-Marquardt")
+        print(f"        a={params[0]}")
+        print(f"        b={params[1]}")
+        print(f"        c={params[2]}")
+        print("\n    Best fitting parameters using scipy.optimize.curve_fit")
+        print(f"        a={popt[0]}")
+        print(f"        b={popt[1]}")
+        print(f"        c={popt[2]}")
+
+        row = i//2
+        col=i%2
+        axs[row,col].set(title=fr"$M_h \approx 10^{{{11+i}}} M_{{\odot}}/h$",
+                        xlabel="x",
+                        ylabel=r"N/$\langle N_\text{sat}\rangle$",
+                        xscale="log",
+                        yscale="log",
+                        xlim=(1e-4, 5),
+                        ylim=(1e-3, 2*max(hist_scaled))
+                       )
+
+        axs[row,col].stairs(hist_scaled, edges=edges)
+        axs[row,col].plot(xx, model(xx, *params), c="r", label="Levenberg-Marquardt")
+        axs[row,col].plot(xx, model(xx, *popt), c="k", ls="--", label="scipy curve_fit")
+    
+    axs[0,0].legend()
+    axs[2,1].set_visible(False)
+
+    fig.tight_layout()
+    fig.savefig(f"figures/subplots_fitted", bbox_inches="tight", dpi=600)
 
 if __name__ in ("__main__"):
     main()
