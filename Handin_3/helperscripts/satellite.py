0f6c9d0d17d1025ae18f5c56fbe20897b472239a
Stan <s_verhoeve@hotmail.nl>
2025-04-14 09:39:53 +0200

    Added param cache. Added option to not use integral (bad but fast)

--- Handin_3/helperscripts/satellite.py
+++ Handin_3/helperscripts/satellite.py
@@ -36,11 +36,28 @@ def n(
     """
     return A * Nsat * ((x / b) ** (a - 3)) * np.exp(-((x / b) ** c))
 
+# Chache parameters to avoid doing the integration multiple times
+_cached_params = {"abc": None, "Z": None, "dZ": None}
+def _rounded_params(a, b, c, digits=12):
+    """Round params to digits"""
+    return (round(a, digits), round(b, digits), round(c, digits))
+
+def get_partition_and_derivatives(a, b, c):
+    """Get cached parameters"""
+    global _cached_params
+    abc = _rounded_params(a, b, c)
+
+    if _cached_params["abc"] != abc:
+        # Cache miss, recompute
+        _cached_params["abc"] = abc
+        _cached_params["Z"] = partition(a, b, c)
+        _cached_params["dZ"] = dpartition_dparams(a, b, c)
+
+    return _cached_params["Z"], _cached_params["dZ"]
 
 def galaxy_dist(x, Nsat, a, b, c):
     """Non-normalised galaxy dist"""
     return 4 * np.pi * Nsat * x ** (a - 1) * b ** (3 - a) * np.exp(-((x / b) ** c))
-
 
 def dgalaxy_dparam(x, Nsat, a, b, c, which="a"):
     """Derivative of non-normalised dist wrt its params"""
@@ -76,13 +93,13 @@ def dpartition_dparams(a, b, c):
 
 def model(x, Nsat, a, b, c):
     """Normalised model"""
-    return galaxy_dist(x, Nsat, a, b, c) / partition(a, b, c)
+    Z, _ = get_partition_and_derivatives(a, b, c)
+    return galaxy_dist(x, Nsat, a, b, c) / Z
 
 
 def dmodel_dparam(x, Nsat, a, b, c, which="a"):
     """Model derivative wrt one of its params"""
-    dZ = dpartition_dparams(a, b, c)
-    Z = partition(a, b, c)
+    Z, dZ = get_partition_and_derivatives(a, b, c)
     if which == "a":
         extra_term = np.log(x / b)
         dZ = dZ[0]
@@ -97,27 +114,28 @@ def dmodel_dparam(x, Nsat, a, b, c, which="a"):
     return galaxy_dist(x, Nsat, a, b, c) * (extra_term / Z - dZ / Z**2)
 
 
-def bin_function(func, binedges):
+def bin_function(func, binedges, use_integral=True):
     """Bin function given binedges"""
     N = len(binedges) - 1
     result = np.zeros(N)
-
-    centers = 0.5 * (binedges[1:] + binedges[:-1])
-    result = func(centers) * np.diff(binedges)
-    # for i in range(N):
-    #     result[i] = romberg(func, (binedges[i], binedges[i + 1]), m=ORDER)
+    
+    if not use_integral:
+        centers = 0.5 * (binedges[1:] + binedges[:-1])
+        return func(centers) * np.diff(binedges)
+    for i in range(N):
+        result[i] = romberg(func, (binedges[i], binedges[i + 1]), m=ORDER)
 
     return result
 
 
-def binned_model(binedges, Nsat, a, b, c):
+def binned_model(binedges, Nsat, a, b, c, use_integral=True):
     """Binned galaxy model"""
     func = lambda x: model(x, Nsat, a, b, c)
-    return bin_function(func, binedges)
+    return bin_function(func, binedges, use_integral)
 
 
-def dmodel_dparams_binned(binedges, Nsat, a, b, c, which):
+def dmodel_dparams_binned(binedges, Nsat, a, b, c, which, use_integral=True):
     """Binned derivative wrapper"""
     dm_dp = lambda x: dmodel_dparam(x, Nsat, a, b, c, which=which)
 
-    return bin_function(dm_dp, binedges)
+    return bin_function(dm_dp, binedges, use_integral)
