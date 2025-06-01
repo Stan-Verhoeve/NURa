88aa814717add1624420e642cea89c1af265fe4a
Stan <s_verhoeve@hotmail.nl>
2025-06-01 02:29:34 +0200

    Removed explicit loop. Should be faster now

--- NURb/Handin/helperscripts/fft.py
+++ NURb/Handin/helperscripts/fft.py
@@ -1,32 +1,55 @@
 import numpy as np
-from copy import deepcopy
 
-def fft(array, N=None):
+def fft(array):
     array = np.asarray(array, dtype=np.complex64)
+    
+    def recurse(array):
+        N = array.shape[0]
+        
+        if N <= 1:
+            return array
 
-    if not N:
-        N = array.size
+        # Enter recursion on even and odd indices  
+        even = recurse(array[::2])
+        odd = recurse(array[1::2])
+        
+        # Compute exponent term for all k
+        exp_term = np.exp(-2j * np.pi * np.arange(N // 2) / N).astype(np.complex64)
+        # Exponent term is only multiplied by odd indices
+        WH = exp_term * odd
+        
+        # The values 't' are the even indices
+        return np.concatenate([even + WH, even - WH])
+    
+    return recurse(array)
 
-    if N > 2:
-        even = deepcopy(array[::2])
-        odd = deepcopy(array[1::2])
+def ifft(array):
+    array = np.asarray(array, dtype=np.complex64)
+    N = array.shape[0]
 
-        array[: N // 2] = fft(even, N=N // 2)
-        array[N // 2 :] = fft(odd, N=N // 2)
+    def recurse(array):
+        N = array.shape[0]
+        if N <= 1:
+            return array
 
-    for k in range(N // 2):
-        exp_term = np.exp(2j * np.pi * k / N)
-        WH = exp_term * array[k + N // 2]
-        t = deepcopy(array[k])
-        array[k] = t + WH
-        array[k + N // 2] = t - WH
+        # Enter recursion on even and odd indices
+        even = recurse(array[::2])
+        odd = recurse(array[1::2])
 
-    return array
+        # Compute exponent term for all k
+        exp_term = np.exp(2j * np.pi * np.arange(N // 2) / N).astype(np.complex64)
+        # Exponent term is only multiplied by odd indices
+        WH = exp_term * odd
+
+        # The values 't' are the even indices
+        return np.concatenate([even + WH, even - WH])
+    
+    return recurse(array) / N
 
 def fftn(array):
     array = np.asarray(array, dtype=np.complex64)
     ndim = array.ndim
-    
+
     # Iterate over number of dimensions
     for axis in range(ndim):
         # In principle, FFTN(A) = FFT(FFT(FFT(A, axis=0), axis=1), axis=2, ...)
@@ -38,33 +61,33 @@ def fftn(array):
         # over every other dimension.
 
         # As such, we wish to reshape our array s.t. we have our axis of interest
-        # in one dimension, as the first axis of the reshaped array, and all the 
-        # other axes squished in the other dimension. We can then iterate over our 
-        # axis of interest to perform the 1D fourier tranforms on the individual 1D 
+        # in one dimension, as the first axis of the reshaped array, and all the
+        # other axes squished in the other dimension. We can then iterate over our
+        # axis of interest to perform the 1D fourier tranforms on the individual 1D
         # slices of the data
 
         # As an example in 3D, instead of doing
         # for col in range(array.shape[1]):
         #     for height in range(array.shape[2]):
         #         fft(array[:, col, height])
-        # 
+        #
         # We instead do
         # for col in range(reshaped.shape[1]):
         #     fft(reshaped[:, col])
 
         # We use np.moveaxis(array, origin, destination) to quickly move the axis
-        # of interest to the front, and reshape to be 2D. 
+        # of interest to the front, and reshape to be 2D.
 
         array = np.moveaxis(array, axis, 0)
         shape = array.shape
         array = array.reshape(shape[0], -1)
-        
+
         for i in range(array.shape[1]):
             array[:,i] = fft(array[:,i])
 
         array = array.reshape(shape)
         array = np.moveaxis(array, 0, axis)
-    
+
     return array
 
 def ifftn(array):
@@ -86,31 +109,8 @@ def ifftn(array):
 
     return array
 
-def ifft(array, N=None):
-    array = np.asarray(array, dtype=np.complex64)
-    if not N:
-        N = array.size
-    
-    def recurse(array, N):
-        if N > 2:
-            even = deepcopy(array[::2])
-            odd = deepcopy(array[1::2])
-
-            array[: N // 2] = recurse(even, N=N // 2)
-            array[N // 2 :] = recurse(odd, N=N // 2)
-
-        for k in range(N // 2):
-            exp_term = np.exp(-2j * np.pi * k / N)
-            WH = exp_term * array[k + N // 2]
-            t = deepcopy(array[k])
-            array[k] = t + WH
-            array[k + N // 2] = t - WH
-
-        return array
-
-    array = recurse(array, N)
-    return array / N
-
-def fftfreq(size, samplerate):
+def fftfreq(size, d):
     indices = np.arange(size)
-    return (samplerate * (indices - size // 2)) / size
+    midpoint = size // 2
+    indices[indices > midpoint] -= size
+    return indices / (d * size)
